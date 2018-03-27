9f95812ee0c51065addb0261fa5906d87858ba95
bishoy <bishoybasily@gmail.com>
2018-03-27 09:36:28 +0200

    cleaning

--- processor/src/main/java/com/yolo/processor/Annotations.java
+++ processor/src/main/java/com/yolo/processor/Annotations.java
@@ -3,7 +3,7 @@ package com.yolo.processor;
 public interface Annotations {
 
 	String
-			ENABLE_WORKER = "EnableWorker",
-			ENABLE_GRAPH = "EnableGraph";
+			ENABLE_WORKER = "com.yolo.annotations.EnableWorker",
+			ENABLE_GRAPH = "com.yolo.annotations.EnableGraph";
 
 }
