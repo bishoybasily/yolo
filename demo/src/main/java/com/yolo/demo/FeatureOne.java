a4a92d93a269ebcf9e8f4a053ef5d181e4896c19
bishoy <bishoybasily@gmail.com>
2018-03-31 17:33:39 +0200

    qualifier & constructor injection works missing scopes

--- demo/src/main/java/com/yolo/demo/FeatureOne.java
+++ demo/src/main/java/com/yolo/demo/FeatureOne.java
@@ -8,6 +8,12 @@ import com.yolo.annotations.Qualifier;
 @Configuration
 public class FeatureOne {
 
+	private Token token;
+
+	public FeatureOne(Token token) {
+		this.token = token;
+	}
+
 	@Bean
 	public ActivityOne activityOne(ServiceRegistration serviceRegistration, @Qualifier("oracle") Database database) {
 		return new ActivityOne(serviceRegistration, database);
