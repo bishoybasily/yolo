abb8c9d8ae65de42b93b4bcf5f6b6621f4f5ec04
bishoybasily <bishoybasily@gmail.com>
2018-07-15 23:12:08 +0200

    before closing to android

---
+++ aspects/src/main/java/com/gmail/bishoybasily/yolo/aspects/Trace.java
@@ -0,0 +1,14 @@
+package com.gmail.bishoybasily.yolo.aspects;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.SOURCE)
+public @interface Trace {
+
+
+}
